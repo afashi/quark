6cc4dd6801f846bdb77d95b97ab52fcf3b85f0bc
ha <afa_shi@163.com>
2019-03-15 17:47:59 +0800

    init

---
+++ quark-auth/auth-server/src/main/java/authserver/AuthServerApplication.java
@@ -0,0 +1,16 @@
+package authserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author zhou
+ */
+@SpringBootApplication
+public class AuthServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AuthServerApplication.class, args);
+    }
+
+}
